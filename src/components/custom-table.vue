56c1f9f1f9ed1aba97c871429eaa411b80f88784
kcs <dr_cskuang@163.com>
2019-11-14 19:38:28 +0800

    添加自定义组件

---
+++ src/components/custom-table.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div>
+      <custom-collapse :searchFlag="false">
+        <slot name="search"></slot>
+      </custom-collapse>
+    </div>
+    <div>
+      <el-table :data="data" :header-cell-style="$tableCellHeader" stripe>
+        <el-table-column type="selection" v-if="isSelection" width="50"></el-table-column>
+        <el-table-column :index="$indexMethod(page)" align="center"
+                         label="序号" type="index" v-if="isIndex" width="50">
+        </el-table-column>
+        <template v-if="columns.length">
+          <template v-for="column of columns">
+            <el-table-column :formatter="$formatDateTime" :label="column.label" :prop="column.key"
+                             v-if="isTime(column.key)"></el-table-column>
+            <el-table-column :label="column.label" :prop="column.key" v-else="isTime(column.key)"></el-table-column>
+          </template>
+        </template>
+        <template v-else>
+          <template v-for="(item, key, index) of data.length ? data[0] : {}">
+            <el-table-column :formatter="$formatDateTime" :label="key" :prop="key" v-if="isTime(key)"></el-table-column>
+            <el-table-column :label="key" :prop="key" v-else="isTime(key)"></el-table-column>
+          </template>
+        </template>
+        <el-table-column align="center" label="操作" v-if="isOperation">
+          <template slot-scope="scope">
+            <slot :row="scope.row" name="operation"></slot>
+          </template>
+        </el-table-column>
+      </el-table>
+      <custom-paging :page="page" @size-change="handleSizeChange" @start-change="handleCurrentChange"
+                     v-show="data.length"></custom-paging>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import CustomPaging from '@/components/custom/custom-paging.vue';
+
+    class Column {
+        key!: string;
+        label!: string;
+    }
+
+    @Component({
+        components: {CustomPaging}
+    })
+    export default class CustomTable extends Vue {
+        public name: string = 'CustomTable';
+        @Prop({default: false})
+        public isSelection?: boolean;
+        @Prop({default: false})
+        public isIndex?: boolean;
+        @Prop({default: false})
+        public isOperation?: boolean;
+        @Prop({default: () => []})
+        public data?: any[];
+        @Prop({default: () => []})
+        public columns?: Column[];
+        @Prop({
+            default: {size: 10, start: 1, total: 0}
+        })
+        public page?: any;
+        @Prop({
+            default: {size: 10, start: 1}
+        })
+        public condition?: any;
+
+        isTime(val: string): boolean {
+            let regex = /[^]*Time$|^]*time$/;
+            return regex.test(val);
+
+        }
+
+        handleSizeChange(val: number) {
+            this.condition.size = val;
+            this.$emit('condition-change', this.condition);
+        }
+
+        handleCurrentChange(val: number) {
+            this.condition.start = val;
+            this.$emit('condition-change', this.condition);
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
