0f7756b412e62c6a2f5db0f9cca0a81f724bdd53
kcs <kuangchunsheng@fuweifintech.com>
2019-11-29 15:02:57 +0800

    echarts 使用

--- src/views/index/test.vue
+++ src/views/index/test.vue
@@ -16,51 +16,48 @@
         @Provide()
         public show: boolean = false;
         public option: any = {
-            //标题
             title: {
-                text: '基础雷达图'
+                text: '极坐标双数值轴'
             },
-            tooltip: {},
             legend: {
-                data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
+                data: ['line']
             },
-            radar: {
-                name: {
-                    textStyle: {
-                        color: '#fff',
-                        backgroundColor: '#999',
-                        borderRadius: 3,
-                        padding: [3, 5]
-                    }
-                },
-                indicator: [
-                    { name: '销售（sales）', max: 6500},
-                    { name: '管理（Administration）', max: 16000},
-                    { name: '信息技术（Information Techology）', max: 30000},
-                    { name: '客服（Customer Support）', max: 38000},
-                    { name: '研发（Development）', max: 52000},
-                    { name: '市场（Marketing）', max: 25000}
-                ]
+            polar: {
+                center: ['50%', '54%']
+            },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'cross'
+                }
+            },
+            angleAxis: {
+                type: 'value',
+                startAngle: 0
+            },
+            radiusAxis: {
+                min: 0
             },
             series: [{
-                name: '预算 vs 开销（Budget vs spending）',
-                type: 'radar',
-                data : [
-                    {
-                        value : [4300, 10000, 28000, 35000, 50000, 19000],
-                        name : '预算分配（Allocated Budget）'
-                    },
-                    {
-                        value : [5000, 14000, 28000, 31000, 42000, 21000],
-                        name : '实际开销（Actual Spending）'
-                    }
-                ]
-            }]
+                coordinateSystem: 'polar',
+                name: 'line',
+                type: 'line',
+                showSymbol: false,
+                data: []
+            }],
+            animationDuration: 2000
         };
         mounted() {
             this.initChart();
         }
         public initChart() {
+            let data = [];
+            for (let i = 0; i <= 360; i++) {
+                let t = i / 180 * Math.PI;
+                let r = Math.sin(2 * t) * Math.cos(2 * t);
+                data.push([r, i]);
+            }
+            this.option.series[0].data = data;
             const chart: any = echarts.init(this.$refs.chart as HTMLCanvasElement);
             chart.setOption(this.option);
         }
